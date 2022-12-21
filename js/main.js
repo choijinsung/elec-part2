f7bbac3fcd515e56c1223ff23e3ec7934a8dc568
choijinsung <bugslife102401@nate.com>
2022-12-21 10:25:37 +0900

    이미지와 아이콘 추가

--- js/main.js
+++ js/main.js
@@ -22,3 +22,42 @@ function showBasket() {
 function hideBasket() {
   basketEl.classList.remove("show");
 }
+
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchFromEl = headerEl.querySelector('.search-from');
+const searchCloserEl =  searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchFromEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  })
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600)
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  })
+  searchDelayEls.reverse();
+  searchInputEl.value = '';
+}
